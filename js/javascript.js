ca618c96b8e8758733ab771debb89ce48f47afa2
wahwahwin370 <159791789+wahwahwin370@users.noreply.github.com>
2024-06-08 00:37:17 +0630

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -167,7 +167,9 @@ document.getElementById("the-first-jam").onmouseover = function() {
 function sendMail() {
   var params = {
     name: document.getElementById("name").value,
+      com_name: document.getElementById("com_name").value,
     email: document.getElementById("email").value,
+      phone: document.getElementById("phone").value,
     message: document.getElementById("message").value,
   };
 
@@ -177,7 +179,9 @@ function sendMail() {
     emailjs.send(serviceID, templateID, params)
     .then(res=>{
         document.getElementById("name").value = "";
+        document.getElementById("com_name").value = "";
         document.getElementById("email").value = "";
+        document.getElementById("phone").value = "";
         document.getElementById("message").value = "";
         console.log(res);
         alert("Your message sent successfully!!")
