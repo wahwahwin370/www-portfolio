e81619e955ebeb354d9f5ba3813903258ea71345
wahwahwin370 <159791789+wahwahwin370@users.noreply.github.com>
2024-06-08 14:43:07 +0630

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -170,7 +170,7 @@ button.disabled = true;
 input.addEventListener("change", stateHandle);
 
 function stateHandle() {
-    if(document.querySelector(".input").value === "") {
+    if(document.querySelector("input").value === "") {
         button.disabled = true;
     } else {
         button.disabled = false;
